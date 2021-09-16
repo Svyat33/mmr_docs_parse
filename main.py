b0d7577041e9446844884c04597e5afdc658f2bf
Svyat <svyatka@gmail.com>
2021-09-16 18:04:25 +0300

    Добавил примеры и количество документов вынес в переменную окружения.

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import logging
-
+import os
+from dotenv import load_dotenv
 from parse.api import send_document
 from parse.link import DecisionLink, get_one_decision
 
+load_dotenv()
 logger = logging.getLogger(__name__)
 
 if __name__ == '__main__':
 
-    for link in DecisionLink(10):
+    for link in DecisionLink(int(os.getenv('DOC_QTY', 10))):
         try:
             doc = get_one_decision(link)
         except:
             logger.warning(f"Ошибка разбора адреса {link}")
             continue
         res = send_document(doc)
+        print(res.status_code, res.json())
         # Результат отправки на проверку:
         # logger.info(res.json())
